c9b2476039f2884a000fb4b0ce19a611072970d2
Nicolas Hoffmann <dante3333@gmail.com>
2015-02-05 22:37:35 +0100

    Add js-* to class names
    Fixed #2 from @kaelig

--- jquery-hide-show.js
+++ jquery-hide-show.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
 
 
    // loading expand paragraphs
-   if ( $('.expandmore').length  &&  $( ".to_expand" ).length ) { // if there are at least one :)
-      $('.expandmore' ).each( function(index_to_expand) {
+   if ( $('.js-expandmore').length  &&  $( ".js-to_expand" ).length ) { // if there are at least one :)
+      $('.js-expandmore' ).each( function(index_to_expand) {
           var $this = $(this) ,
               index_lisible = index_to_expand+1,
-              $to_expand = $this.next(".to_expand"),
+              $to_expand = $this.next(".js-to_expand"),
               $expandmore_text = $this.html();
           
           $this.html( '<button class="expandmore__button">' + $expandmore_text + '</button>' );
