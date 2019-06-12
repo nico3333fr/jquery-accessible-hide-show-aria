e787af1b98632dd6736810bc4e65162dce9b402f
Nicolas Hoffmann <dante3333@gmail.com>
2019-06-12 12:25:38 +0200

    Version number

--- jquery-accessible-hide-show-aria.js
+++ jquery-accessible-hide-show-aria.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
 
     /*
      * jQuery simple and accessible hide-show system (collapsible regions), using ARIA
-     * @version v1.9.0   
+     * @version v1.9.1   
      * Website: https://a11y.nicolas-hoffmann.net/hide-show/
      * License MIT: https://github.com/nico3333fr/jquery-accessible-hide-show-aria/blob/master/LICENSE
      */
